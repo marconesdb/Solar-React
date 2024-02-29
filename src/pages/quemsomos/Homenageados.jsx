14c188fc4c68771c2d522b533562a01b0bcaf1a3
marconesb <marconebritt@gmail.com>
2024-02-28 21:36:41 -0300

    Add de img

--- src/pages/quemsomos/Homenageados.jsx
+++ src/pages/quemsomos/Homenageados.jsx
@@ -1,38 +1,34 @@
 import React from 'react'
 import Footer from '../../Footer'
 import Navbar from '../../Navbar'
+import homenageado1 from '../quemsomos/img-homenageados/Cunha.jpg';
+import homenageado2 from '../quemsomos/img-homenageados/Sr-Joaquin.jpeg';
 
 const homenageados = [
   { 
     nome: 'Nome do Homenageado 1', 
     descricao: 'Descrição do Homenageado 1',
-    imagemUrl: '/public/imagens/Sr-Joaquin.jpeg'
+    imagem: homenageado1
   },
   { 
     nome: 'Nome do Homenageado 2', 
     descricao: 'Descrição do Homenageado 2',
-    imagemUrl: '/public/imagens/Cunha.jpg'
-  },
-
-  { 
-    nome: 'Nome do Homenageado 2', 
-    descricao: 'Descrição do Homenageado 2',
-    imagemUrl: 'https://example.com/imagem2.jpg'
+    imagem: homenageado2
   },
   // Adicione mais homenageados conforme necessário
 ];
+  // Adicione mais homenageados conforme necessário
 export default function Homenageados() {
   return (
     <div>
     <div>
       <Navbar/>
       <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold mt-16 mb-4 text-center">Homenageados</h1>
-      
+      <h1 className="text-3xl font-bold mb-4">Homenageados</h1>
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {homenageados.map((homenageado, index) => (
           <div key={index} className="bg-gray-100 p-4 rounded-lg">
-            <img src={homenageado.imagemUrl} alt={homenageado.nome} className="w-full mb-4 rounded-lg" />
+            <img src={homenageado.imagem} alt={homenageado.nome} className="w-full mb-4 rounded-lg" />
             <h2 className="text-xl font-bold mb-2">{homenageado.nome}</h2>
             <p>{homenageado.descricao}</p>
           </div>
